2c38656d33a642aacda9a763bd8002568e11b39b
Daniil Petelin <DaniilPetelin77@gmail.com>
2020-01-04 18:51:50 +0300

    v 0.1

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 var canvas = document.getElementById('cl');
 var ctx = canvas.getContext('2d');
 var myColor = 'black';
-var mySize = '5';
+var mySize = '10';
 
 document.getElementById('colors').oninput = function() {
     myColor = this.value;
@@ -31,4 +31,19 @@ canvas.onmousedown = function(event) {
     canvas.onmouseup = function() {
         canvas.onmousemove = null;
     }
+};
+
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+    coll[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        var content = this.nextElementSibling;
+        if (content.style.display === "block") {
+            content.style.display = "none";
+        } else {
+            content.style.display = "block";
+        }
+    });
 }
