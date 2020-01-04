836775d54a7c16556853ce01a993cce278f20486
Daniil Petelin <DaniilPetelin77@gmail.com>
2020-01-04 17:51:20 +0300

    beta

--- js/script.js
+++ js/script.js
@@ -1,20 +1,29 @@
 var canvas = document.getElementById('cl');
 var ctx = canvas.getContext('2d');
 var myColor = 'black';
+var mySize = '5';
 
 document.getElementById('colors').oninput = function() {
     myColor = this.value;
+};
+
+document.getElementById('size').oninput = function() {
+    mySize = this.value;
+};
+
+document.getElementById('clear').onclick = function() {
+    ctx.clearRect(0, 0, 1850, 500);
 };
 
 canvas.onmousedown = function(event) {
     canvas.onmousemove = function(event) {
         var x = event.offsetX;
         var y = event.offsetY;
-        ctx.fillRect(x-5, y-5, 10, 10);
+        ctx.fillRect(x-mySize, y-mySize, mySize, mySize);
         ctx.fillStyle = myColor;
         ctx.fill();
     }
-    canvas.onmouseup = function() {
+    canvas.onmouseup = function(event) {
         canvas.onmousemove = null;
     }
 }
