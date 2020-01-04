d0bad184ccdba5d1b73f0a115c345da20dd040ef
Daniil Petelin <DaniilPetelin77@gmail.com>
2020-01-04 13:48:48 +0300

    commit

--- js/script.js
+++ js/script.js
@@ -1,8 +1,13 @@
 var canvas = document.getElementById('cl');
 var ctx = canvas.getContext('2d');
 
-canvas.onmousemove = function(event) {
-    var x = event.offsetX;
-    var y = event.offsetY;
-    ctx.fillRect(x-5, y-5, 10, 10);
+canvas.onmousedown = function(event) {
+    canvas.onmousemove = function(event) {
+        var x = event.offsetX;
+        var y = event.offsetY;
+        ctx.fillRect(x-5, y-5, 10, 10);
+    }
+    canvas.onmouseup = function() {
+        canvas.onmousemove = null;
+    }
 }
