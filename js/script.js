428c1b604a824d9ce3b6ab090f855ae08db80aa6
Daniil Petelin <DaniilPetelin77@gmail.com>
2020-01-04 13:56:34 +0300

    paint

--- js/script.js
+++ js/script.js
@@ -1,11 +1,18 @@
 var canvas = document.getElementById('cl');
 var ctx = canvas.getContext('2d');
+var myColor = 'black';
+
+document.getElementById('colors').oninput = function() {
+    myColor = this.value;
+};
 
 canvas.onmousedown = function(event) {
     canvas.onmousemove = function(event) {
         var x = event.offsetX;
         var y = event.offsetY;
         ctx.fillRect(x-5, y-5, 10, 10);
+        ctx.fillStyle = myColor;
+        ctx.fill();
     }
     canvas.onmouseup = function() {
         canvas.onmousemove = null;
